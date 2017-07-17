cf05c7c4b573241c90de07b6b70d8b48bbcc60df
bindatype <bindatype@gmail.com>
2017-07-17 16:09:51 +0000

    third commit

--- string_tokenizer.c
+++ string_tokenizer.c
@@ -4,37 +4,39 @@
 
 int main (int argc,char **argv)
 {
-        char *token = *(argv+1);
-        int comma_array[100];
-        int hyphen_array[100];
-        int j=0;
-        int k=0;
+	char *token = *(argv+1);
+	int comma_array[100];
+	int hyphen_array[100];
+	int j=0;
+	int k=0;
 
-        for (int i = 0;i<strlen(token)+1;i++)
-        {
-                printf("%c\n",*(token+i));
-                if (*(token+i) == 44)
-                        comma_array[j++]=i;
-                if (*(token+i) == 45)
-                        hyphen_array[k++]=i;
+	for (int i = 0;i<strlen(token)+1;i++)
+	{
+		printf("%c\n",*(token+i));
+		if (*(token+i) == 44)
+			comma_array[j++]=i;
+		if (*(token+i) == 45)
+			hyphen_array[k++]=i;
 
-        }
+	}
 
-        char **chunk = malloc( (j+1)* sizeof(char*));
+	char **chunk = malloc( (j+1)* sizeof(char*));
 
 	int sum = 0;
-        for (int i = 0;i<j;i++)
-        {
-                printf(",%d %d %c\n",i,comma_array[i],token[comma_array[i]]);
-                *(chunk+i) = token+sum;
-                sum = token[comma_array[i]];
-        }
 
-        for (int i = 0;i<k;i++)
-        {
-                printf("-%d %d %c\n",i,hyphen_array[i],token[hyphen_array[i]]);
-        }
+	printf("\tIndex Location Character \n");
+	for (int i = 0;i<j;i++)
+	{
+		printf("Comma:  %d \t%d \t%c\n",i,comma_array[i],token[comma_array[i]]);
+		*(chunk+i) = token+sum;
+		sum = token[comma_array[i]];
+	}
 
-        printf("\n");
-        return 0;
+	for (int i = 0;i<k;i++)
+	{
+		printf("Hyphen: %d \t%d \t%c\n",i,hyphen_array[i],token[hyphen_array[i]]);
+	}
+
+	printf("\n");
+	return 0;
 }
